823810dd6ec07824e096083b4eab9c4587bd523d
delta_black5 <matt@kmgks.com>
2025-03-27 20:46:20 -0500

    packages installed

--- 02-Challenge/Bun_Develop/server/src/routes/api/user-routes.ts
+++ 02-Challenge/Bun_Develop/server/src/routes/api/user-routes.ts
@@ -1,23 +1,27 @@
 import express from 'express';
 const router = express.Router();
-import {
-  createUser,
-  getSingleUser,
-  saveBook,
-  deleteBook,
-  login,
-} from '../../controllers/user-controller.js';
+import { asyncHandler } from '../../utils/asyncHandler';
+import { createUser, login, getSingleUser, saveBook, deleteBook } from '../../controllers/user-controller';
+import { authenticateToken } from '../../middleware/auth';
+
+
+export const asyncHandler =
+  (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
 
 // import middleware
 import { authenticateToken } from '../../services/auth.js';
 
+const router = express.Router();
 // put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authenticateToken, saveBook);
+router.route('/').post(asyncHandler(createUser)).put(authenticateToken, asyncHandler(saveBook));
 
-router.route('/login').post(login);
+router.route('/login').post(asyncHandler(login));
 
-router.route('/me').get(authenticateToken, getSingleUser);
+router.route('/me').get(authenticateToken, asyncHandler(getSingleUser));
 
-router.route('/books/:bookId').delete(authenticateToken, deleteBook);
+router.route('/books/:bookId').delete(authenticateToken, asyncHandler(deleteBook));
 
 export default router;
